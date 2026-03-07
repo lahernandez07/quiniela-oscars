e86219b2665e225f8410074f7dfc356fe88f39f1
lahernandez07 <59944692+lahernandez07@users.noreply.github.com>
2026-03-06 22:25:43 -0600

    Update supabaseBrowser.ts

--- src/lib/supabaseBrowser.ts
+++ src/lib/supabaseBrowser.ts
@@ -1,7 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabaseBrowser = () =>
-  createClient(
+export function supabaseBrowser() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+}
